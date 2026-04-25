ee8012d659c1ac7a1f3e32c8f8a14016689de116
bynueman <youremail@example.com>
2026-04-25 12:53:37 +0700

    Security: Excluded /api/bot from CSRF protection

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->validateCsrfTokens(except: [
+            'api/bot',
+            'api/bot/*'
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
